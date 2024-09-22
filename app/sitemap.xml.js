7853582c5d5e4b7e73ec45f40037e2feebb69e60
SEO Blog <darrenthirtle571@gmail.com>
2024-09-21 18:11:48 -0700

    first commit

--- app/sitemap.xml.js
+++ app/sitemap.xml.js
@@ -1,15 +1,70 @@
-export default function handler(req, res) {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/xml')
-    // Instructing the Vercel edge to cache the file
-    res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
-    // generate sitemap here
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="<a href="http://www.sitemaps.org/schemas/sitemap/0.9">http://www.sitemaps.org/schema...</a>"> 
-    <url>
-      <loc><a href="http://www.myexample.com/foo.html<">http://www.myexample.com/foo.h...</a>;/loc>
-      <lastmod>2022-01-01</lastmod>
-    </url>
-    </urlset>`
-  res.end(xml)
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  // Function to fetch blog posts
+  async function fetchBlogPosts() {
+    const res = await fetch(
+      `https://ausbachi.shop/api/allcategoryblogs`
+    );
+    const data = await res.json();
+    console.log("all blogs list data....", data?.data?.data);
+    return data?.data?.data;
+  }
+
+  // Fetch the blog posts
+  const blogPosts = await fetchBlogPosts(); // Ensure `blogPosts` is fetched correctly
+
+  // Static pages that don't change
+  const staticPages = ["/"];
+
+  // Dynamically generate URLs for blog posts
+  const dynamicPages = blogPosts.map((post) => {
+    return {
+      loc: `https://gaullacltd.co.uk/${post?.category?.slug}/${post.blogkey}`, // Dynamic blog post URL
+      lastmod: post.updated_at, // Last modified date of the post
+      image: post?.images[0]?.image_path,
+    };
+  });
+
+  // Combine static and dynamic pages
+  const allPages = [
+    ...staticPages.map((page) => ({
+      loc: `https://gaullacltd.co.uk/${page}`,
+      lastmod: new Date().toISOString(),
+    })),
+    ...dynamicPages,
+  ];
+
+  // Generate the XML format for the sitemap with image namespace
+  const sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+      ${allPages
+        .map((page) => {
+          return `
+            <url>
+              <loc>${page.loc}</loc>
+              <lastmod>${page?.lastmod}</lastmod>
+              <changefreq>daily</changefreq>
+              <priority>0.7</priority>
+         ${
+           page.image
+             ? `<image:image>
+                      <image:loc>https://gaullacltd.co.uk/${page.image}</image:loc>
+                    </image:image>`
+             : ""
+         }
+            </url>
+          `;
+        })
+        .join("")}
+    </urlset>
+  `;
+
+  // Return the response as XML
+  return new NextResponse(sitemap, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
